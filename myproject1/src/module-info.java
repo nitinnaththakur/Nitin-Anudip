85a472e4c4d180ed5c8f57c1ae185bb73309c581
nitinnaththakur <117462712+nitinnaththakur@users.noreply.github.com>
2022-12-12 16:13:15 +0530

    Add files via upload

---
+++ myproject1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ishan
+ *
+ */
+module myproject1 {
+}
---
+++ myproject1/src/myproject1/HelloWorld.java
@@ -0,0 +1,91 @@
+package myproject1;
+import java.util.Scanner;
+
+
+// odd number beween 1 to 20
+
+//public class HelloWorld {
+//	public static void main(String[] args) {
+//        int b = 20;
+//        System.out.println("Odd number from 1 to 20 :");
+//        for (int i = 0; i <= b; i++) {
+//            if(i % 2 != 0 )//if remainder is zero then the number is prime
+//            System.out.println(i + " ");
+//        }
+//    }
+//}
+
+
+//*************************************************************************************
+
+
+// To find the greatest of the three number
+
+//public class HelloWorld{
+//public static void main(String[] args) {
+//    int a = 10;
+//    int b = 20;
+//    int c = 30;
+//    if (a > b) {
+//        if (a > c) {
+//            System.out.println("Greatest number:" +a);
+//        } else {
+//            System.out.println("Greatest number:" +c);
+//        }
+//    } else {
+//        if (b > c) {
+//            System.out.println("Greatest number:" +b);
+//        } else {
+//            System.out.println("Greatest number:" +c);
+//        }
+//       
+//    }
+//}
+
+
+
+//***********************************************************************************
+
+//To perform operation on two number using switch case
+//public class HelloWorld {
+//	
+//	public static void main(String[] args )
+//	{
+//	System.out.print("Enter two numbers: \n");
+//	Scanner obj= new Scanner(System.in);
+//	double first = obj.nextDouble();
+//	double second = obj.nextDouble();
+//	System.out.print("Enter an operator (+, -, *, /): \n");
+//	char operator = obj.next().charAt(0);
+//	double result;
+//	switch(operator)
+//	{
+//	case '+':
+//	result = first + second;
+//	break;
+//	case '-':
+//	result = first - second;
+//	break;
+//	case '*':
+//	result = first * second;
+//	break;
+//	case '/':
+//	result = first / second;
+//	break;
+//	// operator doesn't match any case constant (+, -, *, /)
+//	default:
+//	System.out.printf("Error!operator is not correct");
+//	return;
+//	}
+//	System.out.printf("%.1f %c %.1f =%.1f", first, operator, second, result);
+//	}
+//	}
+
+
+
+
+
+
+
+
+
